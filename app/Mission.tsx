585b746f465825baae4820be1e4a9952d9dd2035
Philani Ncube <ncbphi001@gmail.com>
2023-12-01 10:40:40 +0200

    Add values to Mission component

--- app/Mission.tsx
+++ app/Mission.tsx
@@ -23,6 +23,39 @@ const links = [
   },
 ];
 
+const values = [
+  {
+    title: "Integrity",
+    list: [
+      "Ethical Behavior",
+      "No corruption",
+      "Capital stewardship",
+      "Honesty"
+    ]
+  },
+  {
+    title: "Ubuntu",
+    list: [
+      "Dignity and respect for each other, communities, environment, diversity and culture",
+      "People first (Batho Pele)",
+    ]
+  },
+  {
+    title: "Accountability",
+    list: [
+      "Professionalism, inclusivity and transparency",
+      "Responsibility",
+    ]
+  },
+  {
+    title: "Passion For Development",
+    list: [
+      "Pro Poor",
+      "Selflessness",
+    ]
+  },
+]
+
 const Mission = () => {
   return (
     <Container>
@@ -31,28 +64,49 @@ const Mission = () => {
           <div>
             <h2 className="text-3xl font-semibold text-aspire">Vision</h2>
             <p className="mt-4 text-lg text-gray-500">
-              ASPIRE: A prosperous and smart Amathole District
+              ASPIRE’s vision alignment contextualises the Districts’ vision
+              within an implementation model Amathole District Municipality:
+              Commitment towards selfless, excellent and sustainable service to
+              all our communities.
+            </p>
+            <p className="mt-4 text-lg text-gray-500">
+              ASPIRE: A PROSPEROUS AND SMART AMATHOLE DISTRICT
             </p>
           </div>
           <div>
             <h2 className="text-3xl font-semibold text-aspire">Mission</h2>
             <p className="mt-4 text-lg text-gray-500">
               The catalyst to promote and implement sustainable spatial and
-              social economic development in the Amathole District
+              social economic development in the Amathole District.
             </p>
           </div>
         </div>
-        <div className="grid grid-cols-1 gap-6 mt-12 md:grid-cols-2 lg:grid-cols-3">
-          {links.map((link, index) => (
-            <div key={index} className="flex items-start w-full gap-2">
-              <span>{link.icon}</span>
-              <div className="flex-1">
-               <h4 className="text-lg font-semibold text-aspire lg:text-2xl">{link.title}</h4>
-                <p className="mt-2 text-gray-500 lg:pr-16">{link.text}</p>
-                <div className="mt-4">
-                 {/* <Link href={link.href} className="px-6 py-2 mt-3 text-white transition-all duration-200 rounded-lg cursor-pointer bg-aspire hover:bg-yellow-700">Read More</Link> */}
-                </div>
-              </div>
+        <div className="grid grid-cols-1 gap-6 mt-12 md:grid-cols-2 lg:grid-cols-4">
+          {/* {links.map((link, index) => ( */}
+          {/* <div key={index} className="flex items-start w-full gap-2"> */}
+          {/* <span>{link.icon}</span> */}
+          {/* <div className="flex-1"> */}
+          {/* <h4 className="text-lg font-semibold text-aspire lg:text-2xl"> */}
+          {/* {link.title} */}
+          {/* </h4> */}
+          {/* <p className="mt-2 text-gray-500 lg:pr-16">{link.text}</p> */}
+          {/* <div className="mt-4"> */}
+          {/* <Link href={link.href} className="px-6 py-2 mt-3 text-white transition-all duration-200 rounded-lg cursor-pointer bg-aspire hover:bg-yellow-700">Read More</Link> */}
+          {/* </div> */}
+          {/* </div> */}
+          {/* </div> */}
+          {/* ))} */}
+
+          {values.map((value, index) => (
+            <div key={index} className="w-full shadow">
+              <h4 className="text-lg font-semibold text-center bg-aspire lg:text-2xl">
+                {value.title}
+              </h4>
+              <ul className="px-8 py-3 mt-2 text-sm text-gray-500 list-disc">
+                {value.list.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
             </div>
           ))}
         </div>
