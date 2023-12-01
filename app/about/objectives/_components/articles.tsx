bfb0a55be65f75f9ff8f7cb2c95913ee09cb300b
Philani Ncube <ncbphi001@gmail.com>
2023-12-01 21:32:22 +0200

    Add Articles component to about/objectives/page

---
+++ app/about/objectives/_components/articles.tsx
@@ -0,0 +1,160 @@
+import Container from "@/components/ui/Container";
+
+const Articles = () => {
+  return (
+    <section>
+      <Container>
+        <div className="grid gap-4 md:grid-cols-3">
+          <article className="bg-white shadow-sm rounded-xl dark:bg-gray-800">
+            <div className="p-6">
+              <h2 className="text-2xl font-semibold">
+                Establish ASPIRE as a world-leading development institution
+              </h2>
+
+              <p className="mt-4 text-xs text-gray-600 dark:text-gray-300">
+                ASPIRE’s opportunity exists in positioning the district as a
+                globally influential and impactful region. International best
+                practice promoted by institutions such as the United Nations,
+                World Economic Forum, World Bank, etc. have integrated
+                development initiatives with drivers for change. ASPIRE, in
+                establishing itself as world-leading development institution,
+                will position the district as a leader in development on a SMART
+                rural vision and mandate. Key areas of focus will be the
+                implementation of best practice methodologies at an
+                implementation level across all Development Pillars. ASPIRE will
+                also aim to promote the district at an international level,
+                through best practice, as a leader in piloting global solutions
+                to rural areas using SMART approaches. The success of ASPIRE’s
+                mandate is linked to strong domestic and international
+                partnerships that will unlock the technical skills and resources
+                needed to effect implementation of interventions. A service-led
+                productive entity will ensure consistent alignment to the
+                districts’ vision, as well as transparent and accountable
+                administration to local municipalities, civil society, and
+                private sector.
+              </p>
+            </div>
+          </article>
+          <article className="bg-white shadow-sm rounded-xl dark:bg-gray-800">
+            <div className="p-6">
+              <h2 className="text-2xl font-semibold">
+                Promote Innovative industry value chain enterprise growth
+              </h2>
+
+              <p className="mt-4 text-xs text-gray-600 dark:text-gray-300">
+                Regional industry value chain enterprise growth will be realised
+                through the stimulation growth and expansion of existing value
+                chains, and introduction of new value chains, within the
+                district. The fourth industrial revolution has seen significant
+                changes to industry value chains, as well as the introduction of
+                new industries. Historically, transformation of these value
+                chains has been difficult. The disruption created by the fourth
+                industrial revolution has created an opportunity for the
+                district to leap-frog development and transformation of these
+                value chains. Hence, ASPIRE will facilitate the transformation
+                of new generation value chains through enterprise development
+                initiatives, transformational partnerships domestically and
+                internationally. ASPIRE will market the district as an
+                international ‘sand- box’ for research and development for SMART
+                rural solutions within the scope of the Development Pillars.
+                Research and development will provide a catalyst for growth,
+                expansion and introduction of new value chains birthed within
+                the district. Innovative research and development and value
+                chain augmentation will contribute to job/skills recalibration
+                to future industry needs. Hence, the District citizen will have
+                the opportunity to enter, and be aligned to future industry
+                skills needs increasing work opportunities. Government, private
+                sector, and citizens will have the opportunity to cohesively
+                grow into the fourth industrial revolution optimally.
+              </p>
+            </div>
+          </article>
+          <article className="bg-white shadow-sm rounded-xl dark:bg-gray-800">
+            <div className="p-6">
+              <h2 className="text-2xl font-semibold">
+                Optimize the socio-economic value of District-owned
+                infrastructure assets
+              </h2>
+
+              <p className="mt-4 text-xs text-gray-600 dark:text-gray-300">
+                The assets of the district serve as a critical socio-economic
+                game changer. ASPIRE will facilitate the link between current
+                assets and their socio-economic benefit, to innovative
+                partnerships. There is a lack of both social and economic
+                infrastructure and high backlog issues for the development
+                nodes. Limited resources for new infrastructure require
+                innovative approaches to infrastructure development. The
+                Infrastructure Pillar covers rural roads, transport and logistic
+                networks improvement, and rural service provision, whilst Social
+                and Human Development covers coordinating and improving access
+                to quality services, as well as building vibrant, caring, and
+                cohesive communities. The district will realise socio- economic
+                revenue from repurposed or rehabilitated assets. ASPIRE will
+                implement high-impact infrastructure transformation projects
+                which will be managed and implemented using global best practice
+                methodologies, and secured through sound investment
+                partnerships. Key projects will include District-wide
+                environmentally sustainable innovative road connectivity,
+                renewable energy, waste management and water security, etc.
+              </p>
+            </div>
+          </article>
+          <article className="bg-white shadow-sm rounded-xl dark:bg-gray-800">
+            <div className="p-6">
+              <h2 className="text-2xl font-semibold">
+                Promote ADM as a domestic and international investment location
+              </h2>
+
+              <p className="mt-4 text-xs text-gray-600 dark:text-gray-300">
+                At a national level, investment within South Africa has been
+                identified as a key area of focus to pursue economic growth, in
+                turn improving the social impact. ASPIRE aims to align to this
+                national imperative by positioning the district as a strategic
+                region for investment both locally and internationally. By
+                partnering with strategic players, ASPIRE will market the SMART
+                rural model to drive investment into the province. The value of
+                land and agrarian transformation in the rural economy is
+                significant to investors. The promotion of viable partnerships
+                towards production, processing, and exporting of agricultural
+                products will not only address food security, but also stimulate
+                the economy of the region. Hence, improved social and human
+                development will be a tangible outcome.
+              </p>
+              <p className="mt-4 text-xs text-gray-600 dark:text-gray-300">
+                The focus will be on current and new industry sectors pursuing a
+                transformation objective. Fundamental to investment promotion of
+                the region is the ability of the district to improve the ease of
+                doing business. One-stop facilities across the district will
+                provide necessary support and linkages to facilitate
+                investment-driven partnerships. Furthermore, partnerships and
+                co-operation agreements will restore investment value to the
+                districts’ natural, heritage and cultural resources, thereby
+                catalyzing growth in investment.
+              </p>
+            </div>
+          </article>
+          <article className="bg-white shadow-sm rounded-xl dark:bg-gray-800">
+            <div className="p-6">
+              <h2 className="text-2xl font-semibold">
+                Position ADM as a SMART District
+              </h2>
+
+              <p className="mt-4 text-xs text-gray-600 dark:text-gray-300">
+                ADM has the opportunity to be a world leader as Smart District.
+                ASPIRE, over the long term, aims to implement smart solutions to
+                the institutional, physical, social, and economic infrastructure
+                of the district. The goal is to see an improved quality of life
+                for each citizen within the district. Smart solutions will aim
+                to increase accessibility and participation of all citizens into
+                the growth of the district. The district will serve as a
+                replicable model locally and internationally for development.
+              </p>
+
+            </div>
+          </article>
+        </div>
+      </Container>
+    </section>
+  );
+};
+export default Articles;
--- app/about/objectives/page.tsx
+++ app/about/objectives/page.tsx
@@ -1,11 +1,13 @@
 import Container from "@/components/ui/Container";
 import ObjectivesHero from "./_components/objectives-hero";
 import ObjectivesGrid from "./_components/objectives-grid";
+import Articles from "./_components/articles";
 
 const page = () => {
   return <Container>
     <ObjectivesHero />
     <ObjectivesGrid />
+    <Articles />
   </Container>;
 };
 export default page;
