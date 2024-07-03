c6a35146799d46c1571ec737dbd532a512172fbb
Philani Ncube <ncbphi001@gmail.com>
2024-07-03 09:56:06 +0200

    Refactor NewsArticle component and update image layout

--- app/news/NewsArticle.tsx
+++ app/news/NewsArticle.tsx
@@ -9,14 +9,14 @@ type Props = {
 }
 
 const NewsArticle = ({newsItem}:Props) => {
-  return <article className="overflow-hidden rounded-md w-full">
-    <Image src={newsItem.image ? newsItem.image : "/images/news.jpg" } width={1920} height={1280} alt={newsItem.name!} className="w-full aspect-video object-cover" />
+  return <article className="w-full overflow-hidden rounded-md">
+    <Image src={newsItem.image ? newsItem.image : "/images/news.jpg" } width={1920} height={1280} alt={newsItem.name!} className="object-cover object-top w-full aspect-video" />
     <div className="p-3">
       <TypographyH4>{newsItem.name}</TypographyH4>
       <p>Author: {newsItem.author}</p>
 
       <div className="mt-3">
-        <Link href={`/news/${newsItem.slug?.current}`}><Button type="button" className="bg-aspire text-white">Read More</Button></Link>
+        <Link href={`/news/${newsItem.slug?.current}`}><Button type="button" className="text-white bg-aspire">Read More</Button></Link>
       </div>
     </div>
   </article>;
